da86103b2ee19b255035ec6df63f99f482be13d7
antal <antal.horvath.90@gmail.com>
2024-05-18 12:48:33 +0200

    build: add auth0 and lombok dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,12 @@ java {
 	sourceCompatibility = JavaVersion.VERSION_21
 }
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
@@ -18,6 +24,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.okta.spring:okta-spring-boot-starter:3.0.6")
+	annotationProcessor("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
