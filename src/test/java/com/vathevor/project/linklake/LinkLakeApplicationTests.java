2b32f902b113418b38bcaa97ff6616e74da4b3e4
antal <antal.horvath.90@gmail.com>
2024-05-20 22:01:44 +0200

    test: import jdbc client config due to missing autoconfiguration in test

--- src/test/java/com/vathevor/project/linklake/LinkLakeApplicationTests.java
+++ src/test/java/com/vathevor/project/linklake/LinkLakeApplicationTests.java
@@ -2,12 +2,14 @@ package com.vathevor.project.linklake;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestJdbcClientConfig.class) // TODO - Spring JdbcClient should be autoconfigured, why not working? Should be closed by now https://github.com/spring-projects/spring-boot/issues/36579
 @SpringBootTest
 class LinkLakeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
